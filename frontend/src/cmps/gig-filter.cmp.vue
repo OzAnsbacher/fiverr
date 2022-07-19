11fac4bb75c3bbcd24a5d90a32d85d4bf8bb9d1f
RoyLeybovitch <roy.leybo@gmail.com>
2022-07-20 00:18:03 +0300

    added hero and sass

---
+++ frontend/src/cmps/gig-filter.cmp.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="gig-filter">
+        <form @submit.prevent="test" class="search-section">
+            <div>Img will come here...</div>
+            <input @change="setFilter" v-model="filterBy.title" type="text" placeholder='Try "building mobile app"' class="search" />
+            <button class="search-btn">Search</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            filterBy: {
+                title: '',
+                category: '',
+            },
+        }
+    },
+    methods: {
+        setFilter() {
+            this.$emit('setFilter', JSON.parse(JSON.stringify(this.filterBy)))
+        },
+        test() {
+            console.log('testing submit')
+        },
+    },
+    computed: {
+        categories() {
+            // return this.$store.getters.getCategories // Complete later
+        },
+    },
+}
+</script>
