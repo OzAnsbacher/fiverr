2540349b805473458801ebedd3cad896a39d0722
leviTLM <levi.talmazan@yahoo.com>
2022-07-20 15:35:45 +0300

    added gig-details scss file and working on gigdt

--- frontend/src/views/gig-page.vue
+++ frontend/src/views/gig-page.vue
@@ -1,18 +1,41 @@
 <template>
-  <div>
-    <h1>Good</h1>
-    <p>a</p>
+<section class="gig-page main-layout">
+  <div class="info-side">
+    <div class="details-title-info">
+      <!-- <h1>{{ gig.title }}</h1> -->
+      <h1>this is supposed to be the title</h1>
+      <div class="seller-info">
+        <p class="owner-name">ayo lamo</p>
+        <p class="owner-level">omal oya</p>
+      </div>
+    </div>
+
   </div>
+  </section>
 </template>
 
 <script>
+import { gigService } from '../services/gig-service';
+
 export default {
+  name: "gig-detail",
   data() {
-    return {};
+    return {
+      gig: null,
+      images: "",
+      rates: "",
+    };
   },
-  created() {},
+  async created() {
+    var { _id } = this.$route.params;
+    await gigService.getById(_id).then((gig) => {
+      this.gig = gig;
+      this.images = gig.images
+      });
+    },
   methods: {},
-  computed: {},
+  computed: {
+  },
   unmounted() {},
 };
 </script>
