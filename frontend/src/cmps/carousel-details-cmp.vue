f0e8e9740f8c2de3c0dc3e7934843f82065b360c
leviTLM <levi.talmazan@yahoo.com>
2022-07-21 15:18:20 +0300

    gig-details work, added from in jsons,ad caruseldt

---
+++ frontend/src/cmps/carousel-details-cmp.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="carousel">
+        <vueper-slides
+            ref="vueperslides1"
+            :touchable="false"
+            fade
+            :autoplay="false"
+            :bullets="false"
+            @slide="
+                $refs.vueperslides2.goToSlide($event.currentSlide.index, {
+                    emit: false,
+                })
+            "
+        >
+            <vueper-slide v-for="(image, i) in images" :key="i" :image="image">
+                <img :src="(image, i)" alt />
+            </vueper-slide>
+        </vueper-slides>
+
+        <div class="thumbnails">
+            <vueper-slides
+                class="no-shadow thumbnails"
+                ref="vueperslides2"
+                @slide="
+                    $refs.vueperslides1.goToSlide($event.currentSlide.index, {
+                        emit: false,
+                    })
+                "
+                fixed-height="75px"
+                :bullets="false"
+                :touchable="false"
+                :gap="2.5"
+                :arrows="false"
+            >
+                <vueper-slide
+                    v-for="(image, i) in images"
+                    :key="i"
+                    :image="image"
+                    @click.native="$refs.vueperslides2.goToSlide(i)"
+                >
+                    <img :src="(image, i)" alt />
+                </vueper-slide>
+            </vueper-slides>
+        </div>
+    </div>
+</template>
+
+<script>
+import { VueperSlide, VueperSlides } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+
+export default {
+    props: ["images"],
+    data() {
+        return {};
+    },
+    created() {},
+    componets: {
+        VueperSlides,
+        VueperSlide,
+    },
+};
+</script>
+
+<style>
+.carousel {
+    width: 100%;
+}
+
+.vueperslides.vueperslide {
+    height: 400px;
+}
+</style>
--- frontend/src/views/gig-page.vue
+++ frontend/src/views/gig-page.vue
@@ -1,46 +1,100 @@
 <template>
-    <section v-if="gig" class="gig-page main-layout">
-        <div class="info-side">
-            <div class="details-title-info">
-                <div class="gig-details-category">
-                    <p> {{ gig.tags }}  </p>
-                </div>
-                <h1>{{ gig.title }}</h1>
-                <div class="seller-info">
-                    <div class="profile-img">
-                        <img
-                            src="https://fiverr-res.cloudinary.com/t_profile_thumb,q_auto,f_auto/attachments/profile/photo/d2b733c8f152a9baab1b8e2efc14e176-1655401248537/340ea128-2dbd-4524-83c3-321ae8101a44.jpeg"
-                            alt=""
-                            class="seller-img"
-                        />
-                        <p class="owner-name">{{ gig.owner.fullname }}</p>
-                        <p class="owner-level">
-                            level 3 seller{{ gig.owner.level }}
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <p>Price ${{ gig.price }}</p>
-            <p>Days to make {{ gig.daysToMake }}</p>
-            <p><img :src="gig.imgUrl" alt="" /></p>
+  <section v-if="gig" class="gig-page main-layout">
+    <div class="info-side">
+      <div class="details-title-info">
+        <p class="gig-category">{{ gig.category }}</p>
+        <h1>{{ gig.title }}</h1>
+        <div class="seller-info">
+          <div class="profile-img">
+            <img :src="gig.owner.imgUrl" alt="" class="seller-img" />
+          </div>
+          <p class="owner-name">{{ gig.owner.fullname }}</p>
+          <p>Level {{ gig.owner.level }} Seller</p>
         </div>
-        <div class="about-gig">
-            <p class="about-title">About this gig</p>
-            <p>{{ gig.description }}</p>
-            <div class="buy-btn">
-                <button @click="addOrder">Continue (${{ gig.price }})</button>
-            </div>
-            <p class="about-title">About the seller</p>
-            <div class="about-seller">
-                <div class="seller-profile"></div>
-            </div>
+      </div>
+      <carusel-details :images="images"></carusel-details>
+  
+      <div class="about-gig">
+        <p class="about-title">About This Gig</p>
+        <p class="about-text">{{ gig.description }}</p>
+      </div>
+        <img class="main-gig-image" :src="gig.imgUrl" alt="">
+      <p class="about-title">About The Seller</p>
+      <div class="about-seller">
+        <div class="seller-profile">
+          <div class="profile-img-about">
+            <img :src="gig.owner.imgUrl" alt="" class="seller-img" />
+          </div>
         </div>
-    </section>
+        <div class="rate-profile">
+          <p>{{ gig.owner.fullname }}</p>
+          <button class="contact-me-btn">Contact Me</button>
+        </div>
+      </div>
+      <div class="seller-info-details">
+        <div class="info-table">
+          <ul>
+            <li>
+              From
+              <br />
+              <b>{{ gig.owner.country }}</b>
+            </li>
+            <li>
+              Avg. response time
+              <br />
+              <b>Way too long</b>
+            </li>
+            <li>
+              Member since
+              <br />
+              <b>August 1993</b>
+            </li>
+            <li>
+              Last delivery
+              <br />
+              <b>Three months ago</b>
+            </li>
+          </ul>
+        </div>
+        <div class="seller-description">{{ gig.owner.description }}</div>
+      </div>
+        </div>
+    <div class="check-out-section">
+      <div class="check-out-part">
+        <div class="checkout-title">
+          <p class="service">{{ gig.category }}</p>
+          <p class="price">${{ gig.price }}</p>
+        </div>
+        <div class="additional-info">
+          <div class="delivery-wrapper">
+            <div class="img-clock">
+              <img src="../assets/logo/clock.png" alt />
+              <p>{{ gig.daysToMake }} Days Delivery</p>
+            </div>
+          </div>
+        </div>
+        <div class="promises-section">
+          <div class="guarentee-section">
+            <p>High quality product guarenteed</p>
+          </div>
+          <div class="guarentee-section">
+            <p>Delivery on time guarentee</p>
+          </div>
+          <div class="guarentee-section">
+            <p>1 Product</p>
+          </div>
+        </div>
+        <div class="buy-btn">
+          <button>Continue to checkout {{ gig.price }}</button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import { gigService } from "../services/gig-service";
+import carouselDetails from "../cmps/carousel-details-cmp.vue";
 
 export default {
     name: "gig-detail",
@@ -49,6 +103,12 @@ export default {
             gig: null,
             images: "",
             rates: "",
+            currUser: null,
+            reviewToAdd: {
+              txt: "",
+              createdAt: new Date().getMinutes(),
+              flag: "",
+            }
         };
     },
     async created() {
@@ -58,15 +118,19 @@ export default {
         console.log(res);
         this.gig = res;
         this.images = res.images;
+        const userId = this.gig.owner._id; 
     },
     methods: {},
     computed: {
         gigImg() {
-            return this.gig;
+            return this.gig.image;
         },
         // return this.gig and save it in the created
     },
-    unmounted() {},
+    unmounted() { },
+    components: { 
+        carouselDetails
+      }
 };
 </script>
 
