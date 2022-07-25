db58408ab3792ac22cbf79c25215843bdd933684
leviTLM <levi.talmazan@yahoo.com>
2022-07-25 16:29:45 +0300

    pulling fixed header from roy

--- frontend/src/cmps/sixerr-header.cmp.vue
+++ frontend/src/cmps/sixerr-header.cmp.vue
@@ -38,7 +38,6 @@
                     />
                 </div>
                 <nav class="nav">
-<<<<<<< HEAD
                     <router-link to="/explore" class="explore">
                         <a :class="$route.meta.logoClass">Explore</a>
                     </router-link>
@@ -50,43 +49,6 @@
                     <!-- </div> -->
                     <div class="login-modal" v-show="showLogin" @click="closeLogin">
                         <sign-in @toggleLogin="toggleLogin" @closeLogin="toggleLogin" />
-=======
-                    <div class="left-links">
-                        <router-link to="/explore" class="explore">
-                            <a :class="$route.meta.logoClass">Explore</a>
-                        </router-link>
-                        <router-link
-                            :class="$route.meta.logoClass"
-                            to="/"
-                            class="become-seller"
-                            >Become a seller</router-link
-                        >
-
-                        <!-- add functionality inside div tag later -->
-                        <a
-                            class="sign-in"
-                            :class="$route.meta.logoClass"
-                            @click="toggleLogin"
-                            >Sign in</a
-                        >
-                    </div>
-                    <a
-                        class="join"
-                        :class="$route.meta.bodyClass"
-                        @click="toggleSignUp"
-                        >Join</a
-                    >
-
-                    <div
-                        class="login-modal"
-                        v-show="showLogin"
-                        @click="closeLogin"
-                    >
-                        <sign-in
-                            @toggleLogin="toggleLogin"
-                            @closeLogin="toggleLogin"
-                        />
->>>>>>> 14551fd063a642caa3204538c14989723b8931a2
                     </div>
                     <div
                         class="signup-modal"
