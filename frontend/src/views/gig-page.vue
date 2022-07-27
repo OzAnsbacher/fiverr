8605546269d8a7af7f5b22b7b91aa095b057b023
leviTLM <levi.talmazan@yahoo.com>
2022-07-27 12:12:23 +0300

    work on stars + checkout page

--- frontend/src/views/gig-page.vue
+++ frontend/src/views/gig-page.vue
@@ -107,7 +107,6 @@
 
 <template>
     <section v-if="gig" class="gig-page main-layout">
-
         <div class="info-side">
             <div class="details-title-info">
                 <h1>{{ gig.title }}</h1>
@@ -148,8 +147,8 @@
                 </div>
                 <div class="rate-profile">
                     <p>{{ gig.owner.fullname }}</p>
-                    <span class="stars-details"
-                        >{{ getStars }} {{ gig.owner.rate }}
+                    <span class="stars-details" :v-html="getStars"
+                        >{{ gig.owner.rate }}
                         <h1 class="review-length">(1k+)</h1></span
                     >
                     <!-- <img src="getStars" alt=""> -->
@@ -194,7 +193,17 @@
                 <div class="additional-info">
                     <div class="delivery-wrapper">
                         <div class="img-clock">
-                            <img src="" alt />
+                            <svg
+                                width="16"
+                                height="16"
+                                viewBox="0 0 16 16"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 14c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6-2.7 6-6 6z"
+                                ></path>
+                                <path d="M9 4H7v5h5V7H9V4z"></path>
+                            </svg>
                             <p>{{ gig.daysToMake }} Days Delivery</p>
                         </div>
                     </div>
@@ -205,55 +214,61 @@
                 <div class="promises-section">
                     <div class="guarentee-section">
                         <div class="v-image">
-                                <!-- <svg
-                                    width="16"
-                                    height="16"
-                                    viewBox="0 0 11 9"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="currentFill"
-                                    style='<svg width=
-    "16" height="16" viewBox="0 0 11 9" xmlns="http: //www.w3.org/2000/svg" fill="currentFill"><path d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"></path></svg>;
-'
-                                >
-                                    <path
-                                        d="M3.645 8.102.158 4.615a.536.npm536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
-                                    ></path>
-                                </svg> -->
+                            <svg
+                                width="16"
+                                height="16"
+                                viewBox="0 0 11 9"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentFill"
+                            >
+                                <path
+                                    d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
+                                ></path>
+                            </svg>
                         </div>
 
                         <p>High quality product guarenteed</p>
                     </div>
                     <div class="guarentee-section">
                         <div class="v-image">
-                            <!-- <img
-                                src="../img/Check-mark.jfif"
-                                class="v-details"
-                                alt=""
-                            /> -->
+                            <svg
+                                width="16"
+                                height="16"
+                                viewBox="0 0 11 9"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentFill"
+                            >
+                                <path
+                                    d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
+                                ></path>
+                            </svg>
                         </div>
                         <p>Delivery on time guarentee</p>
                     </div>
                     <div class="guarentee-section">
                         <div class="v-image">
-                            <!-- <img
-                                src="../img/Check-mark.jfif"
-                                class="v-details"
-                                alt=""
-                            /> -->
+                            <svg
+                                width="16"
+                                height="16"
+                                viewBox="0 0 11 9"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentFill"
+                            >
+                                <path
+                                    d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
+                                ></path>
+                            </svg>
                         </div>
                         <p>1 Product</p>
                     </div>
                 </div>
 
                 <div class="buy-btn">
-                    
-                        <button v-on:click="loadGig">Continue</button>
-                    
-
+                    <button v-on:click="loadGig">Continue</button>
                 </div>
             </div>
         </div>
-                    <!-- <header class="sub-header block" >
+        <!-- <header class="sub-header block" >
         <p class="sub-header-content max-width-container equal-padding flex">
             <ul class="categories flex clean-list">
                 <li>ayo</li>
@@ -298,17 +313,40 @@ export default {
     },
     methods: {
         loadGig() {
-      this.$router.push("/checkout/" + this.gig._id);
-    },
+            this.$router.push("/checkout/" + this.gig._id);
+        },
     },
     computed: {
         gigImg() {
             return this.gig.image;
         },
         getStars() {
-            let stars = "";
+            // var starsSvg = `<svg
+            //     xmlns="http://www.w3.org/2000/svg"
+            //     viewBox="0 0 1792 1792"
+            //     width="15"
+            //     height="15"
+            // >
+            //     <path
+            //         fill="currentColor"
+            //         d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"
+            //     />
+            // </svg>`.repeat(this.gig.owner.rate);
+            // console.log(starsSvg);
+            // return starsSvg;
+            let stars = '';
             for (let index = 0; index < this.gig.owner.rate; index++) {
-                stars += "★";
+                stars += `<svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1792 1792"
+                width="15"
+                height="15"
+            >
+                <path
+                    fill="currentColor"
+                    d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"
+                />
+            </svg>`;
             }
             return stars;
         },
