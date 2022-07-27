f3238e63adf5d5d3d909e3155966d7cf9102773e
leviTLM <levi.talmazan@yahoo.com>
2022-07-27 18:10:16 +0300

    added checkout page + more work on it

--- frontend/src/views/gig-page.vue
+++ frontend/src/views/gig-page.vue
@@ -110,7 +110,7 @@
     <section v-if="gig" class="gig-page main-layout">
         <div class="info-side">
             <div class="details-title-info">
-                <h1>{{ gig.title }}</h1>
+                <h1 id="overView">{{ gig.title }}</h1>
                 <div class="seller-info">
                     <div class="profile-img">
                         <img :src="gig.owner.imgUrl" alt="seller image" class="seller-img" />
@@ -128,10 +128,10 @@
             </div>
             <carousel-details-cmp :images="images"></carousel-details-cmp>
             <div class="about-gig">
-                <p class="about-title">About This Gig</p>
-                <p class="about-text">{{ gig.about }}</p>
+                <p class="about-title" id="aboutThisGig">About This Gig</p>
+                <p class="about-text">{{ gig.description }}</p>
             </div>
-            <p class="about-title">About The Seller</p>
+            <p class="about-title" id="aboutSeller">About The Seller</p>
             <div class="about-seller">
                 <div class="seller-profile">
                     <div class="profile-img-about">
@@ -207,20 +207,17 @@
                 <div class="promises-section">
                     <div class="guarentee-section">
                         <div class="v-image">
-                            <!-- <svg
-                                    width="16"
-                                    height="16"
-                                    viewBox="0 0 11 9"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="currentFill"
-                                    style='<svg width=
-    "16" height="16" viewBox="0 0 11 9" xmlns="http: //www.w3.org/2000/svg" fill="currentFill"><path d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"></path></svg>;
-'
-                                >
-                                    <path
-                                        d="M3.645 8.102.158 4.615a.536.npm536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
-                                    ></path>
-                                </svg> -->
+                                                        <svg
+                                width="16"
+                                height="16"
+                                viewBox="0 0 11 9"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentFill"
+                            >
+                                <path
+                                    d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
+                                ></path>
+                            </svg>
                         </div>
 
                         <p>High quality product guarenteed</p>
