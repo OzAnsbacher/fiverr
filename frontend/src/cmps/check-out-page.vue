71897a5a5d25636c56c9c5d87b7c8f184f1ef69a
leviTLM <levi.talmazan@yahoo.com>
2022-07-28 13:10:43 +0300

    added reviews + work on checkout page

--- frontend/src/cmps/check-out-page.vue
+++ frontend/src/cmps/check-out-page.vue
@@ -256,7 +256,7 @@ export default {
                                     d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"
                                 ></path>
                             </svg>
-                       Fast delivery time guarenteed
+                       Customer support provided
                         </div>
                                                         <div class="v-image">
                                                         <svg
@@ -342,5 +342,12 @@ export default {
       this.$router.push("/user-profile/" + this.gig._id);
     },
     },
+    computed: {   getStars() {
+            let stars = ''
+            for (let index = 0; index < this.gig.owner.rate; index++) {
+                stars += '★'
+            }
+            return stars
+        },}
 };
 </script>
