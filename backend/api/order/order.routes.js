bc3545c4406810933b0a19fb4c3e74f0359ca50c
RoyLeybovitch <roy.leybo@gmail.com>
2022-07-28 11:11:49 +0300

    back-office upgrades

--- backend/api/order/order.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
-const { getorders, addorder } = require('./order.controller')
-const router = express.Router()
-
-router.get('/', getorders)
-router.post('/', addorder)
-
-module.exports = router
--- backend/config/prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    dbURL: 'mongodb+srv://test:1234@cluster0.lpeu3.mongodb.net/gig_db?retryWrites=true&w=majority',
-}
