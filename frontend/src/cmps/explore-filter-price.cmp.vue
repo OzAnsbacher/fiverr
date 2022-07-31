6f008d64986129e20865f6f6c7e3ac5f7e47d544
Oz Ansbacher <ozansbacher@gmail.com>
2022-07-31 19:33:35 +0300

    filter

--- frontend/src/cmps/explore-filter-price.cmp.vue
+++ frontend/src/cmps/explore-filter-price.cmp.vue
@@ -1,52 +1,82 @@
-    
-   <template>
+<template>
   <section class="budget-filter-container">
-    <el-select class="ex-price ex-btn" @select="filterPrice" placeholder="Budget"
-      :class="{ 'full-filter': min > 0 || max > 0 }">
-      <el-option hidden></el-option>
+    <el-select
+      class="ex-price ex-btn"
+      @select="filterPrice"
+      placeholder="Budget"
+      :class="{ 'full-filter': min > 0 || max > 0 }"
+    >
+      <el-option
+        @click="show = true"
+        :style="show ? { visibility: hidden } : null"
+        hidden
+      ></el-option>
       <section class="budget-filter-container">
         <div class="inps-min-max">
           <div class="input-min">
             <h5>MIN.</h5>
-            <el-input v-model="min" @change="filterPrice" class="flex w-50 m-2" placeholder="Any"
-              :prefix-icon="Search" />
+            <el-input
+              v-model="min"
+              @change="filterPrice"
+              class="flex w-50 m-2"
+              placeholder="Any"
+              :prefix-icon="Search"
+            />
           </div>
           <div class="input-max">
             <h5>MAX.</h5>
-            <el-input class="flex w-50 m-2" placeholder="Any" v-model="max" @change="filterPrice" :prefix-icon="Search"
-              width="40px" />
+            <el-input
+              class="flex w-50 m-2"
+              placeholder="Any"
+              v-model="max"
+              @change="filterPrice"
+              :prefix-icon="Search"
+              width="40px"
+            />
           </div>
         </div>
-        <el-button type="success">Apply</el-button>
       </section>
+      <div class="price-btn-ex flex justify-between">
+        <p class="price-clear" @click="clear">Clear All</p>
+        <el-button class="price-aplly" type="success" @click="filterPrice"
+          >Apply</el-button
+        >
+      </div>
     </el-select>
   </section>
 </template>
- 
-   <script>
+
+<script>
 export default {
   data() {
     return {
+      show: null,
       min: null,
       max: null,
     };
   },
-  created() { },
+  created() {},
   methods: {
     filterPrice() {
+      this.show = true;
       const filter = {
         min: this.min,
         max: this.max,
       };
       this.$emit("price", filter);
     },
+    clear() {
+      this.min = null;
+      this.max = null;
+      filterPrice();
+    },
   },
   computed: {},
-  unmounted() { },
+  unmounted() {},
 };
-</script> 
-    
-  <style  >
+</script>
+
+<style>
 .budget-filter-container {
   display: flex;
   flex-direction: column;
@@ -57,11 +87,9 @@ export default {
   align-items: center;
 }
 .el-select-dropdown__wrap.el-scrollbar__wrap.el-scrollbar__wrap--hidden-default {
-  height: 160px
+  height: 160px;
 }
 .el-select-dropdown__wrap.el-scrollbar__wrap.el-scrollbar__wrap--hidden-default {
   overflow-y: hidden;
 }
-</style>  
-    
-  
+</style>
